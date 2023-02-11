b005c64fc2a157c5e5c5ce739c24cf4f880fe8a8
Yerlin-D <122233359+Yerlin-D@users.noreply.github.com>
2023-02-11 14:30:20 -0400

    Add files via upload

---
+++ Ejemplo 2 - patrones de diseño.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Proxypatron
+
+{
+    class program
+    {
+        // patron de diseño proxy
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+
+            Iservice concreteService = new ConcreteService();
+            Iservice proxy = new proxy (concreteService);
+
+            concreteService.login(15);
+            proxy.login(15);
+
+            concreteService.login(20);
+            proxy.login(20);
+
+            Console.ReadLine();
+
+        }
+
+        interface Iservice
+        {
+            void login(int edad);
+        }
+
+        class ConcreteService : Iservice
+        {
+            public void login(int edad)
+            {
+                Console.WriteLine($"Inicio de sesión satisfactorio. Tu edad es {edad}");
+        }
+
+        }
+
+        class proxy : Iservice
+        {
+            private Iservice _service;
+
+            public proxy(Iservice service)
+            {
+                _service=service;
+            }
+            public void login(int edad)
+            {
+                if (edad<18)
+                {
+                    Console.WriteLine($"Eres menor de edad");
+                }
+                else
+                {
+                    _service.login(edad);   
+                }
+            }
+        }
+    }
+    
+}
---
+++ Ejemplo 3 - patrones de diseño.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer.RealWorld
+{
+ 
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+    
+
+            IBM ibm = new IBM("Banco BHD", 120.00);
+            ibm.Attach(new Investor("Jose"));
+            ibm.Attach(new Investor("Juan"));
+
+           
+
+            ibm.Price = 120.10;
+            ibm.Price = 120.50;
+            ibm.Price = 120.75;
+
+
+            Console.ReadKey();
+        }
+    }
+
+  
+    public abstract class Stock
+    {
+        private string symbol;
+        private double price;
+        private List<IInvestor> investors = new List<IInvestor>();
+
+     
+        public Stock(string symbol, double price)
+        {
+            this.symbol = symbol;
+            this.price = price;
+        }
+
+        public void Attach(IInvestor investor)
+        {
+            investors.Add(investor);
+        }
+
+        public void Detach(IInvestor investor)
+        {
+            investors.Remove(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (IInvestor investor in investors)
+            {
+                investor.Update(this);
+            }
+
+            Console.WriteLine("");
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (price != value)
+                {
+                    price = value;
+                    Notify();
+                }
+            }
+        }
+
+
+        public string Symbol
+        {
+            get { return symbol; }
+        }
+    }
+
+  
+    public class IBM : Stock
+    {
+
+        public IBM(string symbol, double price)
+            : base(symbol, price)
+        {
+        }
+    }
+
+
+    public interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+
+
+    public class Investor : IInvestor
+    {
+        private string name;
+        private Stock stock;
+
+     
+
+        public Investor(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(Stock stock)
+        {
+            Console.WriteLine("Notificar a {0} que {1} " +
+                "cambio a {2:C}", name, stock.Symbol, stock.Price);
+        }
+
+        public Stock Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+    }
+}
